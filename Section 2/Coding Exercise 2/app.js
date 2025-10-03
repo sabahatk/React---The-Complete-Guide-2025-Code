9a04ae2c0a072669910f98388af08a49f9ce3b80
Mohammad Sabahat Khan <sabahatkhan@live.ca>
2025-10-03 16:27:05 -0400

    Create app.js

---
+++ Section 2/Coding Exercise 2/app.js
@@ -0,0 +1,6 @@
+function transformToObjects(numberArray) {
+    // Todo: Add your logic
+    const newArray = numberArray.map((item) => ({val: item}));
+    return newArray;
+    // should return an array of objects
+}
