e90e7e467ec61f03fe4aeff40d0121cadbaf7fd6
Mohammad Sabahat Khan <sabahatkhan@live.ca>
2025-10-03 16:53:55 -0400

    Create app.js

---
+++ Section 2/29. Using Functions as Values/app.js
@@ -0,0 +1,19 @@
+const handleTimeout = () => {
+  console.log("Timed out!");
+};
+
+const handleTimeout2 = () => {
+  console.log("Timed out ... again!");
+};
+
+setTimeout(handleTimeout, 2000);
+setTimeout(handleTimeout2, 3000);
+setTimeout(() => {
+  console.log("More timing out...");
+}, 4000);
+
+function greeter(greetFn) {
+  greetFn();
+}
+
+greeter(() => console.log("Hi"));
