0a9380b20e4701b0982d66349323f8ddb25bf6d9
Mohammad Sabahat Khan <sabahatkhan@live.ca>
2025-10-03 15:29:30 -0400

    Create app.js

---
+++ Section 2/16. import and export/app.js
@@ -0,0 +1,6 @@
+// import { apiKey  } from "./util.js";
+// import apiKey from "./util.js";
+import { apiKey, abc as content } from "./util.js";
+// import * as util from "./util.js";
+
+console.log(util.apiKey);
