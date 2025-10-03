cb592a1841f282ce7f3e058790826673b547e2be
Mohammad Sabahat Khan <sabahatkhan@live.ca>
2025-10-03 16:06:12 -0400

    Create app.js

---
+++ Section 2/20. Arrow Functions/app.js
@@ -0,0 +1,15 @@
+// import { apiKey  } from "./util.js";
+// import apiKey from "./util.js";
+//import { apiKey, abc as content } from "./util.js";
+// import * as util from "./util.js";
+
+//console.log(util.apiKey);
+
+export default function () {
+  console.log("Hello");
+}
+
+export default (userName, message) => {
+    console.log('Hello');
+    return userName + message;
+};
